1db754bf7714897c0cd59296c7269080d5bbde97
AtharvaKharwade <akkharwade@gmail.com>
2026-02-13 19:48:08 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs , not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("Bloodyhell")
+
+// tc39.es this is ecma organization which have decided to set a specific standard for js because of various browser usage (various standards)
+// MdM is also used for checking standards
+
+//numbers => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone values
+//undefined =>
+//symbol => unique
+
+console.log(typeof 32)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
