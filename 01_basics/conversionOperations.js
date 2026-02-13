5d13a7317a0e930473fefa103eb1893d0ea64cf5
AtharvaKharwade <akkharwade@gmail.com>
2026-02-13 20:19:25 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,49 @@
+let score = "33"
+let score2 = "33abs"
+let score3 = null
+let score4 = undefined
+
+/*
+console.log(typeof score)
+console.log(typeof score2)
+console.log(typeof score3)
+console.log(typeof score4)
+*/
+
+
+let covert_into_num = Number(score)
+let covert_into_num2 = Number(score2)
+let covert_into_num3 = Number(score3)
+let covert_into_num4 = Number(score4)
+
+/*
+console.log(typeof covert_into_num)
+console.log(covert_into_num)
+
+console.log(typeof covert_into_num2)
+console.log(covert_into_num2)
+
+console.log(typeof covert_into_num3)
+console.log(covert_into_num3)
+
+console.log(typeof covert_into_num4)
+console.log(covert_into_num4)
+*/
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 ; false => 0
+// null => 0
+// udefined => NaN
+
+let login = 2
+
+let convert_into_bool = Boolean(login)
+
+console.log(convert_into_bool)
+
+// 1 => true ; 0 => false
+// "1" => true
+// "" => false
+// 2 => true
